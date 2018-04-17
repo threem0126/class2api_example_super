24ab5399cfeebab016891390ce96a06c8a2e654a
threem0126 <89479759@qq.com>
2018-04-18 00:20:48 +0800

    添加数据库初始化SQL

--- src/config/test.config.js
+++ src/config/test.config.js
@@ -19,12 +19,12 @@ export const config = {
     redis: {
         host: "127.0.0.1",
         port: 6379,
-        cache_prefx: '_no_set_',
+        cache_prefx: '{cache_prefx}',
         defaultExpireSecond: 10 * 60
     }
 }
 
-if(config.redis.cache_prefx === '_no_set_')
+if(config.redis.cache_prefx === '{cache_prefx}')
     console.error(`请修改config配置中redis的key前缀（${ __dirname }）`)
-if(config.mysql.host === '{ip}')
+if(config.mysql.host === '{host}')
     console.error(`请修改config配置中mysql链接信息（${ __dirname }）`)
